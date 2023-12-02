6e12626d18783efd9279ec7b459fa9505e119b8a
mohatami_ <hatamitekno@gmail.com>
2023-12-02 12:47:50 +0700

    update db in connection

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    connectionString: process.env.SEKOPI_URL + "?sslmode=require",
 })
 
 pool.connect()
